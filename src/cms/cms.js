37115526344ffcd208f4c6e2ede07e92e2f58eca
Jamie Carter <jc2820@hotmail.com>
2020-08-04 14:59:19 +0100

    Add basic template for cms infopage

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,7 @@
 import CMS from 'netlify-cms-app'
 
 import surveyPreview from './preview-templates/surveyPreview'
+import infopagePreview from './preview-templates/infopagePreview'
 
 CMS.registerPreviewTemplate('survey', surveyPreview)
+CMS.registerPreviewTemplate('infopage', infopagePreview)
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,21 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "../header/header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return <main>{children}</main>
 }
 
