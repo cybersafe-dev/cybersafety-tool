3f1ca0896a55cc1c63cbfdaf1d26f8ac06308317
Jamie Carter <jc2820@hotmail.com>
2020-08-04 15:50:23 +0100

    set up initial pages

---
+++ src/pages/dashboard.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout/layout"
+import SEO from "../components/seo"
+
+const Dashboard = ({location}) => {
+    const { state = {} } = location
+    const { user } = state
+    console.log(user);
+    //console.log(location.search.split("="[1]))
+    //const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+    return (
+      <Layout>
+        <SEO title="Your Survey Dashboard" />
+        <h1>Your dashboard</h1>
+        
+        <Link to="/digitalknowledge/">Digital Knowledge</Link> <br />
+        <Link to="/privacy/">Privacy</Link> <br />
+        <Link to="/sharing/">Sharing</Link> <br />
+        <Link to="/communication/">Communication</Link> <br />
+        <Link to="/criticalthinking/">Critical Thinking</Link> <br />
+        <Link to="/responsibleuse/">Responsible Use</Link> <br />
+
+      </Layout>
+    )
+  }
+  export default Dashboard
