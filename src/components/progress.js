a26333e96b6060da227de911076049d108af2158
Jamie Carter <jc2820@hotmail.com>
2020-08-12 20:15:48 +0100

    add dynamic messages for progress bar

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,10 +1,34 @@
 import React from "react"
 import "../styling/progress.css"
 
-const Progress = ({ done, sectionLength }) => {
+const Progress = ({ done, sectionLength, questionMessageData }) => {
   const [style, setStyle] = React.useState({})
+  const [progressMessage, setProgressMessage] = React.useState("")
+  const {
+    surveyProgress,
+  } = questionMessageData.allFile.edges[0].node.childMarkdownRemark.frontmatter
 
-  const progressWidth = done * 200 / sectionLength
+  React.useEffect(() => {
+    if (sectionLength < 3) {
+      setProgressMessage(() => surveyProgress.lessThanThree)
+    } else {
+      switch (done) {
+        case 0:
+          setProgressMessage(() => surveyProgress.first)
+          break
+        case sectionLength - 2:
+          setProgressMessage(() => surveyProgress.penultimate)
+          break
+        case sectionLength - 1:
+          setProgressMessage(() => surveyProgress.last)
+          break
+        default:
+          setProgressMessage(() => surveyProgress.middle)
+      }
+    }
+  }, [done])
+
+  const progressWidth = (done * 200) / sectionLength
 
   setTimeout(() => {
     const newStyle = {
@@ -16,7 +40,7 @@ const Progress = ({ done, sectionLength }) => {
 
   return (
     <section className="progress-container">
-      <p className="progress-message"> Progress Bar </p>
+      <p className="progress-message">{progressMessage}</p>
       <div className="progress">
         <div className="progress-done" style={style}></div>
       </div>
--- src/pages/category.js
+++ src/pages/category.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
@@ -8,9 +9,43 @@ import CategoryProgress from "../components/categoryProgress"
 import DataErrorPage from "../components/dataerror/dataerror"
 
 const Category = props => {
+  const questionMessageData = useStaticQuery(
+    graphql`
+      query {
+        allFile(
+          filter: {
+            sourceInstanceName: { eq: "content" }
+            name: { eq: "hints" }
+          }
+        ) {
+          edges {
+            node {
+              childMarkdownRemark {
+                frontmatter {
+                  surveyHints {
+                    general
+                    last
+                  }
+                  surveyProgress {
+                    first
+                    last
+                    lessThanThree
+                    middle
+                    penultimate
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
   const { state = {} } = props.location
   const { survey, category } = state
   const [currentQ, setCurrentQ] = React.useState(0)
+
   if (!survey) {
     return <DataErrorPage />
   }
@@ -19,7 +54,11 @@ const Category = props => {
   return (
     <Layout>
       <SEO title="Survey" />
-      <Progress done={currentQ} sectionLength={sectionLength} />
+      <Progress
+        done={currentQ}
+        sectionLength={sectionLength}
+        questionMessageData={questionMessageData}
+      />
       <Question
         survey={survey}
         category={category}
@@ -27,7 +66,10 @@ const Category = props => {
         setCurrentQ={setCurrentQ}
         sectionLength={sectionLength}
       />
-      <CategoryProgress currentQ={currentQ} sectionLength={sectionLength} />
+      <CategoryProgress
+        currentQ={currentQ}
+        sectionLength={sectionLength}
+      />
     </Layout>
   )
 }
