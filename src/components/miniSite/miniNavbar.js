86c1563c26c5bb56aa85c5e0cd8703b3f710344c
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-08-17 17:04:41 +0100

    Set up minisite with fixed header and smooth scroll within page

---
+++ src/components/miniSite/miniNavbar.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Logo from "../../images/cybersafe-logo.png"
+import "../../styling/organisation/mininavbar.css"
+import { Link } from "gatsby"
+
+
+const MiniNavbar = () => (
+  <>
+  <div className="navbar">
+  <div className="nav-logo">
+    <img src={Logo} alt="cybersafe logo" className="logo" />
+</div>
+<div className="nav-links">
+        <Link to="/#about" className="nav">About Us</Link>
+        <Link to="/#cybersafety" className="nav">Cybersafety</Link>
+        <Link to="/#cybersafeireland" className="nav">CyberSafeIreland</Link>
+        <Link to="/#contact-us" className="nav">Contact Us</Link>
+        <Link to="/#get-started" className="nav">Get Started</Link>
+      </div>
+      </div>
+  </>
+)
+
+export default MiniNavbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,50 @@
 
 import React from "react"
 import Layout from "../components/layout/layout"
-import Header from "../components/header/header"
 import SEO from "../components/seo"
+import "../styling/organisation/index.css"
 
-const Index = () => {
+import MiniNavbar from "../components/miniSite/miniNavbar"
+
+import Fig1 from "../images/self-assess.svg"
+import Fig2 from "../images/cybersafety.svg"
+import Fig3 from "../images/csi.svg"
+
+const MiniSite = () => {
   return (
+
     <Layout>
-      <Header />
+  <section id="index-container">
+      <MiniNavbar />
       <SEO title="placeholder" />
-
-
+        <section className="site-container">
+        <h1 id="about" >Self-Assessment Tool</h1>
+          <div className="about-section">
+            <p className="intro-text"> Lorem ipsum doodoo daaaa </p>
+            <img src={Fig1} className="fig1"/>
+          </div>
+          <h1 id="cybersafety" >Cybersafety</h1>
+          <div className="cybersafety-section">
+              <img src={Fig2} />
+              <p> Lorem ipsum doodoo daaaa </p>
+          </div>
+            <h1 id="cybersafeireland"> CyberSafeIreland</h1>
+          <div className="csi-section">
+              <p> Lorem ipsum doodoo daaaa </p>
+                <img src={Fig3} />
+          </div>
+          <h1 id="contact-us" className="contact-section">Contact Us</h1>
+          <div className="contact-section">
+              <p> Lorem ipsum doodoo daaaa </p>
+          </div>
+            <h1 id="get-started">Expression of Interest</h1>
+              <div className="eoi-section">
+            <p> Lorem ipsum doodoo daaaa </p>
+          </div>
+        </section>
+</section>
     </Layout>
+
   )
 }
-export default Index
+export default MiniSite
