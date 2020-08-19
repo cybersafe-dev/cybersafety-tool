d43d77013097c1ea9650cc3c13b7f6990ae77895
Jamie Carter <jc2820@hotmail.com>
2020-08-19 16:22:07 +0100

    add components and styling for admin dashboard

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -3,9 +3,12 @@ import { navigate } from "gatsby"
 import { store } from "../../providers/userProvider"
 import useFirebase from "../../firebase"
 
+import SurveyStats from "./surveyStats"
+import "../../styling/app/adminDashboard.css"
+
 const Profile = () => {
   const [user] = React.useContext(store)
-  const { displayName, email } = user
+  const { schoolName, email } = user
 
   const firebase = useFirebase()
 
@@ -16,15 +19,31 @@ const Profile = () => {
   }
 
   return (
-    <div className="">
-      <div className="">
-        <h2 className="">{displayName}</h2>
-        <h3 className="">{email}</h3>
+    <section className="dashboard-body">
+      <div className="user-info">
+        <h1>{schoolName}</h1>
       </div>
-      <button className="" onClick={signOutApp}>
-        Sign out
-      </button>
-    </div>
+      <div className="columns">
+        <article className="user-surveys">
+          <h2>Surveys filled</h2>
+          <SurveyStats />
+        </article>
+        <article className="user-links">
+          <h2>Your survey link</h2>
+          <p>
+            Please send this link to the appropriate number of school Leaders,
+            Teachers and Pupils along with your unique school ID.
+          </p>
+          <p>
+            Once the correct number of surveys have been filled you will be able
+            to submit the responses to CybersafeIreland
+          </p>
+          <button className="logout-button" onClick={signOutApp}>
+            Log out
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 export default Profile
---
+++ src/components/profile/surveyQuotaBox.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+import "../../styling/app/surveyStats.css"
+
+import leaders from "../../images/leader.svg"
+import teachers from "../../images/teacher.svg"
+import pupils from "../../images/pupil.svg"
+
+const SurveyQuotaBox = ({ userType, scores, quota }) => {
+    let thisImage;
+  switch (userType) {
+    case "School Leaders":
+      thisImage = leaders
+      break
+    case "Teachers":
+      thisImage = teachers
+      break
+    case "Pupils":
+      thisImage = pupils
+      break
+    default:
+      return null
+  }
+
+  return (
+    <section className="quota-box">
+      <img src={thisImage} alt={userType} className="quota-box-icon" />
+      <div className="quota-info">
+        <p className="quota-fraction">
+          {scores.length}/{quota}
+        </p>
+        <p>{userType}</p>
+      </div>
+    </section>
+  )
+}
+
+export default SurveyQuotaBox
