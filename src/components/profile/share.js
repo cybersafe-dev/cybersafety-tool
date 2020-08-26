b4656d9e3d24e5787dfc699dd092fce3bc18eed4
Jamie Carter <jc2820@hotmail.com>
2020-08-26 15:10:16 +0100

    add uid as param in copy and share urls from admin dashboard

--- src/components/profile/share.js
+++ src/components/profile/share.js
@@ -3,14 +3,14 @@ import { EmailShareButton } from "react-share"
 
 import "../../styling/app/share.css"
 
-const Share = props => {
+const Share = ({ uid }) => {
   return (
     <>
       <EmailShareButton
         className="email-share"
         subject={`CyberSafeIreland: Self-Assessment Tool for Schools`}
         body={`Welcome to CyberSafeIreland's Self Assessment Tool. Click this link to find the app and follow the on-screen instructions.`}
-        url="https://vigilant-austin-33c3c5.netlify.app/survey/"
+        url={`https://vigilant-austin-33c3c5.netlify.app/survey/?id=${uid}`}
       >
         Share
       </EmailShareButton>
