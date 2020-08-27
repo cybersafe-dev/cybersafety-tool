37201236e61ef5d5efe9ee3f185efc7829c2e2f1
Jamie Carter <jc2820@hotmail.com>
2020-08-27 17:20:50 +0100

    style error pages and add links to correct home routes

--- src/components/dataerror/dataerror.js
+++ src/components/dataerror/dataerror.js
@@ -1,14 +1,20 @@
 import React from "react"
-
 import Layout from "../../components/layout/layout"
+import Header from "../../components/header/header"
 import SEO from "../../components/seo"
+import "../../styling/errorPage.css"
+import unknown from "../../images/unknown.svg"
 
 const DataErrorPage = () => (
   <Layout>
     <SEO title="Data error" />
-    <h1>Data Error</h1>
-    <p>Oops...we've lost your school's id from your dashboard!</p>
-    <p>Please start again from the link you were sent.</p>
+    <Header />
+    <section className="page-container">
+      <h1 className="error-heading">Data Error</h1>
+      <img src={unknown} alt="Question Mark" className="unknown" />
+      <p>Oops...your school's id went missing!</p>
+      <p>Please start your survey again from the full link you were sent.</p>
+    </section>
   </Layout>
 )
 
