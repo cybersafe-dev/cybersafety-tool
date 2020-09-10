897a6386b38adeae4240599c0bec7d251b95b413
Jamie Carter <jc2820@hotmail.com>
2020-09-10 15:25:48 +0100

    centralise text on data error page

--- src/components/dataerror/dataerror.js
+++ src/components/dataerror/dataerror.js
@@ -12,8 +12,8 @@ const DataErrorPage = () => (
     <section className="page-container">
       <h1 className="error-heading">Data Error</h1>
       <img src={unknown} alt="Question Mark" className="unknown" />
-      <p>Oops...your school's id went missing!</p>
-      <p>Please start your survey again from the full link you were sent.</p>
+      <p className="message">Oops...your school's id went missing!</p>
+      <p className="message">Please start your survey again from the full link you were sent.</p>
     </section>
   </Layout>
 )
--- src/pages/404.js
+++ src/pages/404.js
@@ -9,16 +9,15 @@ import unknown from "../images/unknown.svg"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <Header />
     <section className="page-container">
       <h1 className="error-heading">Not Found</h1>
       <img src={unknown} alt="Question Mark" className="unknown" />
-      <p>Sorry, the page you tried to reach does not exist.</p>
-      <p>
+      <p className="message">Sorry, the page you tried to reach does not exist.</p>
+      <p className="message">
         If you were trying to sign your school up for the self-assessment tool{" "}
         <Link to="/">try here</Link>.
       </p>
-      <p>
+      <p className="message">
         If you were sent a survey link to fill in{" "}
         <Link to="/survey">try here instead</Link>.
       </p>
