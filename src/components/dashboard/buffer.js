c4772683f52809a135f16fa64b53074373e68d72
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-09-15 17:14:05 +0100

    Make policy modal responsive

--- src/components/dashboard/buffer.js
+++ src/components/dashboard/buffer.js
@@ -1,15 +1,15 @@
 import React from "react"
-import "../../styling/survey/modal.css"
+import "../../styling/survey/buffer.css"
 
 const Buffer = ({  bufferModalVisible, toggle }) => {
 
   return (
     <div id="bufferModal">
-      <div className="modal-container">
-        <div className="modal-content">
+      <div className="buffer-container">
+        <div className="buffer-content">
           <p>You will have one opportunity to answer each question. Please read the statements carefully and select the answer which matches your opinion best!</p>
         </div>
-        <button onClick={() => toggle(!bufferModalVisible)} className="modal-btn">Let's go</button>
+        <button onClick={() => toggle(!bufferModalVisible)} className="buffer-btn">Let's go</button>
       </div>
     </div>
   )
