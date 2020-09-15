a9f9c27959ae756a3d7b55183d989df9ac740265
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-09-15 17:23:33 +0100

    Restyle buffer modal and make responsive

--- src/components/dashboard/buffer.js
+++ src/components/dashboard/buffer.js
@@ -4,14 +4,16 @@ import "../../styling/survey/buffer.css"
 const Buffer = ({  bufferModalVisible, toggle }) => {
 
   return (
-    <div id="bufferModal">
+
       <div className="buffer-container">
         <div className="buffer-content">
-          <p>You will have one opportunity to answer each question. Please read the statements carefully and select the answer which matches your opinion best!</p>
+          <h2>Just a quick message...</h2>
+          <p>You will have one opportunity to answer each question.</p>
+          <p>Please read the statements carefully and select the answer which matches your opinion best!</p>
         </div>
-        <button onClick={() => toggle(!bufferModalVisible)} className="buffer-btn">Let's go</button>
+        <button onClick={() => toggle(!bufferModalVisible)} className="buffer-btn">enter</button>
       </div>
-    </div>
+
   )
 }
 export default Buffer
