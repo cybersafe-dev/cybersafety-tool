ca1790560b61d867f2a96db1117e924ad46fd0e1
Jamie Carter <jc2820@hotmail.com>
2020-09-15 13:11:33 +0100

    signup posts to CSI sf account

--- src/salesforce.js
+++ src/salesforce.js
@@ -6,16 +6,16 @@ export const addNewSalesforceLead = (
   rollNumber
 ) => {
   var urlencoded = new URLSearchParams()
-  // This id will need to come from the client's SF org web to lead generator
   urlencoded.append("oid", process.env.GATSBY_SF_OID)
   urlencoded.append("retURL", "http://")
   urlencoded.append("first_name", firstName)
   urlencoded.append("last_name", lastName)
   urlencoded.append("email", email)
   urlencoded.append("company", company)
-  // This id will need to come from the client's SF org web to lead generator
-  // urlencoded.append("00N4K000003FxTE", rollNumber) test value
+  // urlencoded.append("00N4K000003FxTE", rollNumber) test org value
   urlencoded.append("00N1i000002IJKt", rollNumber)
+  urlencoded.append("lead_source", "CYBERSAFE TOOL for SCHOOLS")
+
 
   // console.log(...urlencoded);
 
