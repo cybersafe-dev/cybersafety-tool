e0db99c6f48ab0eec86fe7227f8de83df5050a0c
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-11-25 16:46:14 +0000

    Rewrite preview with new changes

--- content/minisite.md
+++ content/minisite.md
@@ -2,10 +2,10 @@
 minisitecontent:
   tool: >-
     CyberSafe Tool for Schools is a free online tool. It is an opportunity for you to self-evaluate your school’s level of cybersafety against best practice.
-    
+
     Once you click on the ‘Get Started’ button at the top, you will be asked to register by email, and you’re good to go! Relative to your school size your dashboard will give you a specific number of leaders, teachers and pupils who must complete the short online survey, which takes no more than 15 minutes. Once the surveys are complete you can submit the results and we do the rest.
   awards: >-
-    All schools receive a notification of their level award and a completion certificate. If you want to display your school’s commitment to online safety you can then purchase one of our two CyberSafe Tool forSchools ‘award mark’ packages. Read on for more information.
+    All schools receive a notification of their level award and a completion certificate. If you want to display your school’s commitment to online safety you can then purchase one of our two CyberSafe Tool for Schools ‘award mark’ packages. Read on for more information.
   levels: >-
     We have 3 levels of award for schools depending on the results of the survey: CyberAware, CyberSmart and CyberChampion. You can display this award mark on your school website, social media channels and other school communication for 12 months. The marks you receive will be dates and in full colour.
   pricing: >-
--- src/cms/preview-templates/miniSitePreview/miniSitePreview.js
+++ src/cms/preview-templates/miniSitePreview/miniSitePreview.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../../../components/layout/layout"
 import "../../../styling/app/index.css"
 import PropTypes from "prop-types"
-
+import ReactMarkdown from "react-markdown"
 import Fig1 from "../../../images/self-assess.svg"
 import Fig2 from "../../../images/cybersafety.svg"
 import Fig3 from "../../../images/csi.svg"
@@ -13,39 +13,42 @@ const MiniSitePreview = ({ entry }) => {
   if (data) {
     return (
       <Layout>
-        <section id="index-container">
-          <section className="site-container">
-            <div id="about">
-              <h1>Self-Assessment Tool</h1>
+      
+        <section className="section-container">
+          <div id="tool">
+            <div className="section">
+              <h1 className="section-title">The Tool: <span className="italics">What is it?</span></h1>
+              <ReactMarkdown className="site-text" source={data.minisitecontent.tool} />
+            </div>
+              <img src={Fig1} alt="" className="fig" />
+            </div>
+          <div id="awards">
+            <div className="section">
+              <h1 className="section-title">Awards: <span className="italics">Why use the tool?</span></h1>
+              <ReactMarkdown className="site-text" source={data.minisitecontent.awards} />
+            </div>
+              <img src={Fig2} alt="" className="fig" />
+            </div>
+          <div id="levels">
+            <div className="section">
+              <h1 className="section-title">Levels: <span className="italics">What are they?</span></h1>
+              <ReactMarkdown className="site-text" source={data.minisitecontent.levels} />
+            </div>
+            </div>
+              <img src={Marks} alt="" className="marks" />
+            <div id="pricing">
               <div className="section">
-                <div className="site-text">
-                  <p> {data.minisitecontent.tool} </p>
-                </div>
-                <img src={Fig1} alt="" className="fig" />
+                <h1 className="section-title">Pricing: <span className="italics">How much does it cost?</span></h1>
+                <ReactMarkdown className="site-text" source={data.minisitecontent.pricing} />
               </div>
             </div>
-
-            <div id="cybersafety">
-              <h1 className="section-title">Cybersafety</h1>
-              <div className="section">
-                <img src={Fig2} alt="" className="fig" />
-                <div className="site-text">
-                <p> {data.minisitecontent.cybersafety} </p>
-                </div>
-              </div>
+          <div id="about">
+            <div className="section">
+              <h1 className="section-title">About us: <span className="italics">Who are we?</span></h1>
+              <ReactMarkdown className="site-text" source={data.minisitecontent.aboutus} />
             </div>
-
-            <div id="cybersafeireland">
-            {/*eslint-disable-next-line*/}
-                <h1 className="section-title">CyberSafeIreland🇮🇪</h1>
-                  <div className="section">
-                    <div className="site-text">
-                    <p> {data.minisitecontent.cybersafeireland} </p>
-                    </div>
-                    <img src={Fig3} alt="" className="fig" />
-                  </div>
+              <img src={Fig3} alt="" className="fig" />
             </div>
-          </section>
         </section>
       </Layout>
     )
