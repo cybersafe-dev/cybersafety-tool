222fd5c4d510d301cb90b93ddc1403798c683109
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-12-01 14:20:25 +0000

    Change to markdown

--- src/pages/survey.js
+++ src/pages/survey.js
@@ -4,6 +4,7 @@ import { ResponseStore } from "../providers/responseProvider"
 import Layout from "../components/layout/layout"
 import Header from "../components/header/header"
 import SEO from "../components/seo"
+import ReactMarkdown from "react-markdown"
 
 import Icon1 from "../images/intro-icon1.svg"
 import Icon2 from "../images/self-assess.svg"
@@ -30,21 +31,21 @@ const SurveyPage = props => {
       <Header />
       <SEO title="Survey Introduction" />
       <section className="intropage-container">
-        <h1 className="welcome"> {data.title} </h1>
+        <h1 className="welcome"> Welcome! Fáilte! </h1>
         <img src={BgImg} alt="background design" className="bg1" />
         <img src={Half} alt="background design" className="bg2" />
         <img src={Half} alt="background design" className="bg3" />
         <div className="introtext-box">
          <img src={Icon1} alt="" className="figure" />
-         <p className="para-1"> {data.body.paragraph1} </p>
+         <ReactMarkdown className="para-1" source={data.body.paragraph1} />
        </div>
        <div className="introtext-box-horiz-flipped">
-         <p className="para-2"> {data.body.paragraph2} </p>
+         <ReactMarkdown className="para-2" source={data.body.paragraph2} />
          <img src={Icon2} alt="" className="figure" />
        </div>
        <div className="introtext-box">
          <img src={Icon3} alt="" className="figure" />
-         <p className="para-3"> {data.body.paragraph3} </p>
+         <ReactMarkdown className="para-3" source={data.body.paragraph3} /> 
        </div>
         <div className="button-and-pills">
           <Link to="/survey/roleselection/" className="start-btn">
