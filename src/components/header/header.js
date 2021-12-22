c3e59c53df7bf92b2271c9b1f1822962a92ce175
Jamie Carter <jc2820@hotmail.com>
2021-12-22 15:51:23 +0000

    add language toggle to standard header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,12 @@
 import React from "react"
+import LanguageToggle from "../dashboard/languageToggle"
 import Logo from "../../images/cybersafekids-logo.png"
 import "./header.css"
 
 const Header = () => (
   <header className="app-header">
     <img src={Logo} alt="cybersafe logo" className="logo" />
+    <LanguageToggle />
   </header>
 )
 
