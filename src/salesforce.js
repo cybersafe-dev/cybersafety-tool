a0130a5cc9acd68162a0c9a9263d22b927587ad8
Jamie Carter <jc2820@hotmail.com>
2022-02-14 14:04:32 +0000

    new CORS friendly web to lead form works

--- src/salesforce.js
+++ src/salesforce.js
@@ -1,4 +1,4 @@
-export const addNewSalesforceLead = (
+export const oldAddNewSalesforceLead = (
   firstName,
   lastName,
   email,
@@ -17,9 +17,12 @@ export const addNewSalesforceLead = (
   urlencoded.append("company", company)
   urlencoded.append(process.env.GATSBY_SF_ROLL_NUMBER_ID, rollNumber)
   urlencoded.append("lead_source", "CYBERSAFE TOOL for SCHOOLS")
-  urlencoded.append(process.env.GATSBY_SF_TOOL_STATUS_ID, `Conducting surveys from: ${dateNow}`)
+  urlencoded.append(
+    process.env.GATSBY_SF_TOOL_STATUS_ID,
+    `Conducting surveys from: ${dateNow}`
+  )
 
-  console.log(...urlencoded);
+  console.log(...urlencoded)
 
   fetch("https://webto.salesforce.com/servlet/servlet.WebToLead?", {
     body: urlencoded,
@@ -28,10 +31,64 @@ export const addNewSalesforceLead = (
       "Content-type": "application/x-www-form-urlencoded",
     },
   })
-    // .then(res => {
-    //   console.log(res)
-    // })
-    // .catch(error => {
-    //   console.error(error)
-    // })
+  // .then(res => {
+  //   console.log(res)
+  // })
+  // .catch(error => {
+  //   console.error(error)
+  // })
+}
+
+// Web to Lead function
+export const addNewSalesforceLead = (
+  firstName,
+  lastName,
+  email,
+  company,
+  rollNumber
+) => {
+
+  let oid = process.env.GATSBY_SF_OID
+  let rollNumberId = process.env.GATSBY_SF_ROLL_NUMBER_ID
+  let toolStatusId = process.env.GATSBY_SF_TOOL_STATUS_ID
+
+  const sfLead = {
+    oid: oid,
+    retURL: "http://",
+    first_name: firstName,
+    last_name: lastName,
+    email: email,
+    company: company,
+    lead_source: "CYBERSAFE TOOL for SCHOOLS",
+  }
+
+  let d = new Date()
+  let dateNow = d.toDateString()
+  sfLead[rollNumberId] = rollNumber
+  sfLead[toolStatusId] = `Conducting surveys from: ${dateNow}`
+
+  console.log(sfLead)
+
+  let customHiddenIframeName = "SF_WEB_TO_LEAD"
+  if (!document.getElementById(customHiddenIframeName)) {
+    let theiFrame = document.createElement("iframe")
+    theiFrame.id = customHiddenIframeName
+    theiFrame.name = customHiddenIframeName
+    theiFrame.src = "about:blank"
+    theiFrame.style.display = "none"
+    document.body.appendChild(theiFrame)
+  }
+  let form = document.createElement("form")
+  form.method = "POST"
+  form.action = "https://webto.salesforce.com/servlet/servlet.WebToLead?"
+  form.setAttribute("target", customHiddenIframeName)
+  for (let dataPoint in sfLead) {
+    let theInput = document.createElement("input")
+    theInput.name = dataPoint
+    theInput.value = sfLead[dataPoint]
+    theInput.setAttribute("type", "hidden")
+    form.appendChild(theInput)
+  }
+  document.body.appendChild(form)
+  form.submit()
 }
